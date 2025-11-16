8be9b508d1f40f577371354fff24cbb74c07c049
Harry <hcs@dhcp-10-29-140-137.dyn.MIT.EDU>
2025-11-16 17:02:04 -0500

    gd

--- src/gd.py
+++ src/gd.py
@@ -35,7 +35,7 @@ def main(
     lr: float,
     max_steps: int,
     neigs: int = 0,
-    physical_batch_size: int = 1000,
+    physical_batch_size: int = -1,
     eig_freq: int = -1,
     iterate_freq: int = -1,
     regions_freq: int = -1,
@@ -59,7 +59,8 @@ def main(
     title: str = "",
 ):
     # directory = get_gd_directory(dataset, lr, arch_id, seed, opt, loss, beta)
-    path = get_gd_path(dataset, lr, arch_id, seed, opt, loss, beta, beta1, beta2, adam_epsilon)
+    path = get_gd_path(dataset, lr, arch_id, seed, opt,
+                       loss, beta, beta1, beta2, adam_epsilon)
     # print(f"output directory: {directory}")
 
     # makedirs(directory, exist_ok=True)
@@ -74,10 +75,12 @@ def main(
     network = load_architecture(arch_id, dataset).cuda()
 
     torch.manual_seed(7)
-    projectors = torch.randn(nproj, len(parameters_to_vector(network.parameters())))
+    projectors = torch.randn(nproj, len(
+        parameters_to_vector(network.parameters())))
 
     if opt == "adam":
-        optimizer = Adam(network.parameters(), lr=lr, betas=(beta1, beta2), eps=adam_epsilon)
+        optimizer = Adam(network.parameters(), lr=lr,
+                         betas=(beta1, beta2), eps=adam_epsilon)
     else:
         optimizer = get_gd_optimizer(network.parameters(), opt, lr, beta)
 
@@ -87,24 +90,32 @@ def main(
         torch.zeros(max_steps),
         torch.zeros(max_steps),
     )
-    iterates = torch.zeros(max_steps // iterate_freq if iterate_freq > 0 else 0, len(projectors))
+    iterates = torch.zeros(
+        max_steps // iterate_freq if iterate_freq > 0 else 0, len(projectors))
     eigs = torch.zeros(max_steps // eig_freq if eig_freq >= 0 else 0, neigs)
-    regions_pier = torch.zeros((max_steps // regions_freq if regions_freq >= 0 else 0), 3)
+    regions_pier = torch.zeros(
+        (max_steps // regions_freq if regions_freq >= 0 else 0), 3)
     regions_hanin = torch.zeros_like(regions_pier)
     regions_humayan = torch.zeros_like(regions_pier)
+
+    if physical_batch_size == -1:
+        physical_batch_size = len(train_dataset)
 
     for step in range(0, max_steps):
         # if step == 3000:
         #     lr = 0.01
+        #
         train_loss[step], train_acc[step] = compute_losses(
             network, [loss_fn, acc_fn], train_dataset, physical_batch_size
         )
-        test_loss[step], test_acc[step] = compute_losses(network, [loss_fn, acc_fn], test_dataset, physical_batch_size)
+        test_loss[step], test_acc[step] = compute_losses(
+            network, [loss_fn, acc_fn], test_dataset, physical_batch_size)
 
         if opt == "adam":
             if step > 0 and eig_freq != -1 and step % eig_freq == 0:
                 nu = get_adam_nu(optimizer)
-                P = (1 - beta1**step) * ((nu / (1 - beta2**step)).sqrt() + adam_epsilon)
+                P = (1 - beta1**step) * \
+                    ((nu / (1 - beta2**step)).sqrt() + adam_epsilon)
                 eigs[step // eig_freq, :] = get_hessian_eigenvalues(
                     network, loss_fn, abridged_train, neigs=neigs, physical_batch_size=physical_batch_size, P=P
                 )
@@ -149,7 +160,8 @@ def main(
             print("Humayan Regions: ", regions_humayan[step // regions_freq])
 
         if iterate_freq != -1 and step % iterate_freq == 0:
-            iterates[step // iterate_freq, :] = projectors.mv(parameters_to_vector(network.parameters()).cpu().detach())
+            iterates[step // iterate_freq, :] = projectors.mv(
+                parameters_to_vector(network.parameters()).cpu().detach())
 
         # if save_freq != -1 and step % save_freq == 0:
         #     save_files(directory, [("eigs", eigs[:step // eig_freq]), ("iterates", iterates[:step // iterate_freq]),
@@ -165,8 +177,6 @@ def main(
         #     print("Hit goal")
         #     break
 
-        if physical_batch_size == -1:
-            physical_batch_size = len(train_dataset)
         optimizer.zero_grad()
         for X, y in iterate_dataset(train_dataset, physical_batch_size):
             loss = loss_fn(network(X.cuda()), y.cuda()) / len(train_dataset)
@@ -197,12 +207,17 @@ def main(
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train using gradient descent.")
-    parser.add_argument("dataset", type=str, choices=DATASETS, help="which dataset to train")
-    parser.add_argument("arch_id", type=str, help="which network architectures to train")
-    parser.add_argument("loss", type=str, choices=["ce", "mse"], help="which loss function to use")
+    parser = argparse.ArgumentParser(
+        description="Train using gradient descent.")
+    parser.add_argument("dataset", type=str, choices=DATASETS,
+                        help="which dataset to train")
+    parser.add_argument("arch_id", type=str,
+                        help="which network architectures to train")
+    parser.add_argument("loss", type=str, choices=[
+                        "ce", "mse"], help="which loss function to use")
     parser.add_argument("lr", type=float, help="the learning rate")
-    parser.add_argument("max_steps", type=int, help="the maximum number of gradient steps to train for")
+    parser.add_argument("max_steps", type=int,
+                        help="the maximum number of gradient steps to train for")
     parser.add_argument(
         "--opt",
         type=str,
@@ -237,14 +252,16 @@ if __name__ == "__main__":
         type=float,
         help="terminate training if the train loss ever crosses this value",
     )
-    parser.add_argument("--neigs", type=int, help="the number of top eigenvalues to compute")
+    parser.add_argument("--neigs", type=int,
+                        help="the number of top eigenvalues to compute")
     parser.add_argument(
         "--eig_freq",
         type=int,
         default=-1,
         help="the frequency at which we compute the top Hessian eigenvalues (-1 means never)",
     )
-    parser.add_argument("--nproj", type=int, default=0, help="the dimension of random projections")
+    parser.add_argument("--nproj", type=int, default=0,
+                        help="the dimension of random projections")
     parser.add_argument(
         "--iterate_freq",
         type=int,
@@ -279,7 +296,8 @@ if __name__ == "__main__":
     parser.add_argument("--num_hanin_line_samples", type=int, default=10)
     parser.add_argument("--num_hanin_point_samples", type=int, default=10)
     parser.add_argument("--num_humayan_samples", type=int, default=10)
-    parser.add_argument("--num_humayan_orthonormal_vectors", type=int, default=10)
+    parser.add_argument("--num_humayan_orthonormal_vectors",
+                        type=int, default=10)
     parser.add_argument("--title", type=str, default="")
 
     args = parser.parse_args()
