d9902bc7d8019ba90f01329a2bb2fa1b4f6adfcd
hacosi <harrysillifant13@icloud.com>
2025-11-26 01:02:51 -0500

    moving animation debug

--- src/viz.py
+++ src/viz.py
@@ -183,6 +183,7 @@ def make_live_animation(
         x = np.arange(1, i + 1)
         x_eigs = np.arange(1, i + 1)
         x_regions = np.arange(1, i + 1)
+        breakpoint()
         ax_loss.plot(x, history["train_loss"]
                      [: (i + 1) * eig_freq], label="train")
         ax_loss.plot(x, history["test_loss"]
