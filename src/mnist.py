15304d6135b76feb17758b6b81e6413cba9ed1b0
hacosi <harrysillifant13@icloud.com>
2025-12-01 22:32:22 -0500

    experiments

--- src/mnist.py
+++ src/mnist.py
@@ -11,7 +11,7 @@ os.makedirs(DATASETS_FOLDER, exist_ok=True)
 def load_mnist(loss: str) -> (TensorDataset, TensorDataset):
     mnist_train = MNIST(root=DATASETS_FOLDER, download=True, train=True)
     mnist_test = MNIST(root=DATASETS_FOLDER, download=True, train=False)
-    print(mnist_train.unsqueeze(-1).data.shape)
+    print(mnist_train.data.unsqueeze(-1).shape)
     X_train, X_test = (
         flatten(mnist_train.data / 255),
         flatten(mnist_test.data / 255),
